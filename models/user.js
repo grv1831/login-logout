3013e1ae7f8b14228b7b2ae135250123db2b8ab3
GAURAV KUMAR <153052732+grv1831@users.noreply.github.com>
2025-07-01 21:14:13 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://1831gkumar:grv%40123@cluster0.l8fn9i7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://1831gkumar:grv%40123@cluster0.l8fn9i7.mongodb.net/login?retryWrites=true&w=majority&appName=Cluster0";
 
 const userSchema = mongoose.Schema({
   username: String,
